5ce3b3e8f2ea9158ae5ebb3b8b59be0b9e6c8bbe
Gavin Sherman <50465646+SGTWaffles-F34@users.noreply.github.com>
2024-07-01 17:12:46 -0700

    Add files via upload

---
+++ List Builder/src/main/java/com/raccoon_theory/list_builder/ListBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.raccoon_theory.list_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListBuilderApplication.class, args);
+	}
+
+}
---
+++ List Builder/src/test/java/com/raccoon_theory/list_builder/ListBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raccoon_theory.list_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
